2d9486368bccb427835f8f5608dd48013e6eab39
ekanata14 <ekanata1411@gmail.com>
2023-11-24 23:19:44 +0800

    Setting up API to connect to FE

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import API from './src/api/index';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [test, setTest] = useState(0);
+  const [dataUsers, setDataUsers] = useState([]);
+
+  const fetchDataUsers = async () => {
+    try{
+      const response = await API.get('/user'); 
+      setDataUsers(response.data);
+    } catch(err){
+      console.error('Error: ', err);
+    }
+  }
+
+  useEffect(() => {
+    fetchDataUsers();
+  });
 
   return (
     <>
-      <h1>Hello World</h1>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <h1>Testing POS API</h1>
+      {/* {console.log(dataUsers)} */}
+      <table>
+        <thead>
+          <th>Username</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Password</th>
+        </thead>
+        <tbody>
+          {dataUsers.map((user, index) => (
+            <tr key={index}>
+              <td>{user.username}</td>
+              <td>{user.name_user}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
 export default App
